6f3c2634ad349a8199d64375646fd4ad55a44f8e
Dominik Rohal <rohal.dominik.123@gmail.com>
2023-12-18 16:53:17 +0200

    material ui login, secured paths, etc.

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,20 +1,50 @@
-import {useEffect, useState} from "react";
 import loginService from "../services/login"
 import { useDispatch } from "react-redux";
 import {setUser} from "../reducers/loggedUserReducer";
 import {useAuthData} from "../hooks/useAuthHooks";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Navigate } from "react-router-dom";
 
-function LogIn(){
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
 
-  const dispatch = useDispatch()
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function LogIn() {
+  const dispatch = useDispatch();
   const auth = useAuthData()
 
-  const handleLogInClick = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     const credentials = {
-      username: email, password
+      username: data.get('email'), password: data.get('password')
     }
     console.log(credentials)
     const token_data = await loginService.login(credentials)
@@ -23,27 +53,80 @@ function LogIn(){
 
     window.localStorage.setItem(
       "IdealAQConsoleUser",
-      JSON.stringify({currentUser, token: token})
+      JSON.stringify({...currentUser, token: token})
     );
 
-    dispatch(setUser({currentUser, token: token}))
+    dispatch(setUser({...currentUser, token: token}))
 
   }
 
-  return auth.token ? <><p>Logged In</p></> : (
-    <>
-      <h2>Login</h2>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} required/>
-        <br/>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} required/>
-        <br/>
-        <button type="button" onClick={handleLogInClick}>Login</button>
-      </form>
-    </>
-  )
+  return auth.token ? (<Navigate to="/" />) : (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default LogIn
