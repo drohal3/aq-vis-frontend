6a0603b638d8b47d9f4f68e91fbf06a6469ff768
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-13 09:32:08 +0200

    measurements service

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: 'http://127.0.0.1:8080', // replace with your API base URL
+  accept: 'application/json',
+  timeout: 1000,
 });
 
 export default api
