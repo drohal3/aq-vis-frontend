3c07607fee1fea7edb50ec75eb0ecac336710e62
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-13 09:33:06 +0200

    Organisation page base

---
+++ src/pages/Organisation.js
@@ -0,0 +1,23 @@
+import {useAuthData} from "../hooks/useAuthHooks";
+import AppLayout from "../components/AppLayout";
+import Typography from "@mui/material/Typography";
+
+function Organisation(){
+  const auth = useAuthData()
+
+  return (
+    <>
+      <AppLayout>
+        <Typography variant="h4" gutterBottom>
+          Organisation
+        </Typography>
+        <Typography>
+          TODO: restricted access, members management
+        </Typography>
+      </AppLayout>
+      {/*{devices.map((device, key) => (<p key={key}>{device.name}</p>))}*/}
+    </>
+  )
+}
+
+export default Organisation
