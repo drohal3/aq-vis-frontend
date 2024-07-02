460c4f7dacc9c763f61069bea6d1bb04bd617674
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-07-02 14:40:12 +0300

    login error handling

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,21 +1,20 @@
-import loginService from "../services/login"
+import loginService, {LoginError, TokenError} from "../services/login"
 import {setUser} from "../reducers/loggedUserReducer";
 import {useAuthData} from "../hooks/useAuthHook";
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Navigate } from "react-router-dom";
 import logo from '../../public/ideal-aq-logo.png'
-import {Divider} from "@mui/material";
+import {Alert, Divider} from "@mui/material";
 import {useAppDispatch} from "../hooks/hooks.ts";
 import { FormEvent } from "react";
+import {Notifications} from "../components/notifications/Notifications.tsx";
+import {addNotification} from "../utils/notifications.ts"
 
 
 /* eslint-disable-next-line */
@@ -45,91 +44,93 @@ export default function LogIn() {
     const password = data.get('password')
 
     if (!username || !password) {
-      throw Error("Invalid credentials.")
+      addNotification(dispatch, "Invalid credentials!", "error", 3)
+      return
     }
 
     const credentials = {
       username: username.toString(), password: password.toString()
     }
-    const token_data = await loginService.login(credentials)
-    const token = token_data.access_token
-    const currentUser = await loginService.currentUser(token)
-    if (data.get('remember') === 'remember') {
-      console.log("remember me")
-      window.localStorage.setItem("IdealAQConsoleUserToken", token);
+
+    try {
+      const token_data = await loginService.login(credentials)
+      const token = token_data.access_token
+      const currentUser = await loginService.currentUser(token)
+      if (data.get('remember') === 'remember') {
+        console.log("remember me")
+        window.localStorage.setItem("IdealAQConsoleUserToken", token);
+      }
+
+      dispatch(setUser({currentUser, token: token}))
+    } catch (error) {
+      let message = "unknown error"
+      if (error instanceof LoginError) {
+        message = `Error with login: ${error.message}`
+      }
+      if (error instanceof TokenError) {
+        message = `Could not retrieve user with the token!`
+      }
+
+      addNotification(dispatch, message, "error", 5)
     }
-
-    dispatch(setUser({currentUser, token: token}))
-
   }
 
   return auth.token ? (<Navigate to="/" />) : (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <img alt="IdealAQ logo" src={logo} width="70%"/>
-        <Divider sx={{width: '100%', margin: 2,}}/>
-        {/*<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>*/}
-        {/*  <LockOutlinedIcon />*/}
-        {/*</Avatar>*/}
-        <Typography component="h1" variant="h5">
-          Log In
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox name="remember" value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Log In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+      <>
+        <Box sx={{m:2}}>
+          <Notifications />
         </Box>
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+          >
+            <img alt="IdealAQ logo" src={logo} width="70%"/>
+            <Divider sx={{width: '100%', margin: 2,}}/>
+            <Typography component="h1" variant="h5">
+              Log In
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+              />
+              <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+              />
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+              >
+                Log In
+              </Button>
+              <Alert severity="info">
+                Contact <Link sx={{color: "black"}} href="mailto:dominik.rohal@helsinki.fi?subject=[AQvis%20login]%20Issue">admin</Link> if facing a problem with login.
+              </Alert>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </>
   );
 }
--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,6 +1,6 @@
 import {addNotificationToState, removeNotificationFromState, SeverityType} from "../reducers/notificationReducer.ts"
 import {AppDispatch} from "./store.ts";
-export const addNotification = (dispatch:AppDispatch, message:string, severity:SeverityType, delay: number = 2000) => {
+export const addNotification = (dispatch:AppDispatch, message:string, severity:SeverityType, delaySeconds: number = 2) => {
     let timeoutId = -1
 
     const handler = () => {
@@ -9,7 +9,7 @@ export const addNotification = (dispatch:AppDispatch, message:string, severity:S
 
     timeoutId = setTimeout(
         handler,
-        delay
+        delaySeconds * 1000
     )
 
     dispatch(addNotificationToState({
