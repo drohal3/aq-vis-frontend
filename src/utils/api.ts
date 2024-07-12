b31eaf7a7ad5b741d4bf9cbb6e5d68c81385c7b6
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-07-12 20:34:29 +0300

    backend url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-console.log("backend url", import.meta.env.VITE_BACKEND_URL)
-
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8080', // replace with your API base URL
+  baseURL: import.meta.env.VITE_BACKEND_URL, // replace with your API base URL
   // accept: 'application/json',
   timeout: 1000,
 });
